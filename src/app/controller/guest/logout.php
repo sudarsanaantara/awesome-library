182d4197d1173692e3433f7406515b5ea7ec6be6
sudarsanaantara <agung.sudaru@gmail.com>
2025-12-07 06:50:51 +0800

    feat: menggunakan cookie pada sistem login

--- src/app/controller/guest/logout.php
+++ src/app/controller/guest/logout.php
@@ -4,6 +4,8 @@ class Logout extends Controller {
     public function index() {
             session_unset();
             session_destroy();
+            setcookie("id", "");
+            setcookie("key", "");
             header("Location: " . BASEURL);
             exit();
     }
--- src/app/model/User_model.php
+++ src/app/model/User_model.php
@@ -1,18 +1,31 @@
 <?php
-class User_model {
+class User_model
+{
     private $tb_name = "tb_user";
     private $db;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->db = new Database;
     }
 
-    public function getAllUsers() {
+    public function getAllUsers()
+    {
         $this->db->query("SELECT * FROM " . $this->tb_name);
         return $this->db->getAllResults();
     }
 
-    public function getUserByUsername($username) {
+    public function getUserById($id)
+    {
+        $this->db->query("SELECT * FROM " . $this->tb_name . " WHERE id=:id");
+        $this->db->bind("id", $id);
+
+        $result = $this->db->getSingleResult();
+        return $result;
+    }
+
+    public function getUserByUsername($username)
+    {
         $this->db->query("SELECT * FROM " . $this->tb_name . " WHERE username=:username");
         $this->db->bind("username", $username);
 
@@ -20,7 +33,8 @@ class User_model {
         return $result;
     }
 
-    public function getUserByEmail($email) {
+    public function getUserByEmail($email)
+    {
         $this->db->query("SELECT * FROM " . $this->tb_name . " WHERE email=:email");
         $this->db->bind("email", $email);
 
@@ -28,7 +42,8 @@ class User_model {
         return $result;
     }
 
-    public function addUser($data) {
+    public function addUser($data)
+    {
         $this->db->query("INSERT INTO " . $this->tb_name . "(fullname, username, email, password, is_admin) VALUES (:fullname, :username, :email, :password, 0);");
         $this->db->bind("fullname", $data['fullname']);
         $this->db->bind("username", $data['username']);
@@ -39,7 +54,8 @@ class User_model {
         var_dump($result);
     }
 
-    public function loginTest($username, $password) {
+    public function loginTest($username, $password)
+    {
         $this->db->query("SELECT * FROM " . $this->tb_name . " WHERE username=:username AND password=:password");
         $this->db->bind("username", $username);
         $this->db->bind("password", $password);
